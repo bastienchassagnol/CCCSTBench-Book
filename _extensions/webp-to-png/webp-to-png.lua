ff9e1e14023f2ebae5ecee505ca1d2c17ba6adae
bastienchassagnol <bastien_chassagnol@laposte.net>
2025-02-12 17:44:29 -0500

    add extension for handling webp formats

---
+++ _extensions/webp-to-png/webp-to-png.lua
@@ -0,0 +1,10 @@
+
+-- This detects .webp images and converts them to .png automatically during rendering.
+function Image(img)
+  if img.src:match("%.webp$") then
+    local png_src = img.src:gsub("%.webp$", ".png")
+    os.execute("magick convert " .. img.src .. " " .. png_src)
+    img.src = png_src
+  end
+  return img
+end
